d6e1109ecc48c06a9c26b1ef6eee845edeafdd82
lajjo <lajjo1992@gmail.com>
2023-04-10 10:02:50 +0200

    namespaced engine and added title

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <vulkan_thing_engine/platform/entry_point.h>
 #include <vulkan_thing_engine/platform/game.h>
 
+using namespace vulkan_thing_engine;
+
 class VulkanThing : public Game {
-    public:
-    VulkanThing() : Game() {
-        std::cout << "Hello, VulkanThing!" << std::endl;
-    }
+public:
+  VulkanThing(std::string title) : Game(std::move(title)) {
+    std::cout << "Hello, VulkanThing!" << std::endl;
+  }
+
+protected:
+  void Update(float deltaTime) override {}
 };
 
-Game* CreateGame() {
-    return new VulkanThing();
+Game *vulkan_thing_engine::CreateGame() {
+  return new VulkanThing("Vulkan Engine");
 }
